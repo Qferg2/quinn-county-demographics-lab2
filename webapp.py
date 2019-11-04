8c66f75ddba6c4cca4e62f9b5fc3a53461d5c3c3
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-11-04 09:35:30 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,5 +3,14 @@ import os
 import json
 app = Flask(__name__)
 
+get_state_options
+    listOfStates = []
+    for county in counties:
+        if 
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=False, port=54321)
