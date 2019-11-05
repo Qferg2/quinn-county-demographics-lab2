1114ec638c8debb66065ce707b27f1402bf420e6
Qferg2 <54326424+Qferg2@users.noreply.github.com>
2019-11-05 09:40:39 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,16 +1,20 @@
-from flask import Flask, request, Markup, render_template, flsh, Markup
+from flask import Flask, request, Markup, render_template, flash, Markup
 import os
 import json
 app = Flask(__name__)
 
-def get_state_options
+@app.route("/")
+def render_main():
+    return render_template('countyDemo.html')
+
+def get_state_options(counties):
     listOfStates = []
     for county in counties:
         if county['State'] not in listOfStates:
             listOfStates.append(county['State'])
             
-    for 
-
-
+    for state in listOfStates:
+        options = options + Markup("<option value=\"" + s + "\">" + s + "</option>")
+        
 if __name__=="__main__":
     app.run(debug=False, port=54321)
